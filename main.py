c2540221e48a36cd24c57d27b54164430450f6fa
YassinWael <yassinwaelmohamed84@gmail.com>
2023-12-31 22:25:50 +0300

    reload via pythonanywhere terminal added

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 # BUY THE API KEY HERE: https://platform.openai.com/signup
 from flask import Flask,request
-import git
+
 
 app = Flask(__name__)
-
-@app.route('/update_server')
-def webhook():
-       
-    repo = git.Repo('/home/Cookarino/mysite/ICT-FAIR-WEBSITE')
-    origin = repo.remotes.origin
-    origin.pull()
-    return 200
-        
 
 
 @app.route("/")
