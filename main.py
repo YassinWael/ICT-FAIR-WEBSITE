ffc2f40a79331e34bf841b1574ba92dd461f7af3
YassinWael <yassinwaelmohamed84@gmail.com>
2023-12-31 21:28:51 +0300

    basic web boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # BUY THE API KEY HERE: https://platform.openai.com/signup
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return '<a target="_blank" href="https://github.com/YassinWael/ICT-FAIR-WEBSITE">Click here</a>'
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
