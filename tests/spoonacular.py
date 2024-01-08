66e230f0a251538e52a48cbd96a7e53158f62ddd
YassinWael <yassinwaelmohamed84@gmail.com>
2024-01-08 12:06:48 +0300

    Added Templates folder + Finished spoonacular api

--- tests/spoonacular.py
+++ tests/spoonacular.py
@@ -10,42 +10,54 @@ ingredients = input("Enter ingredients : ")
 url = "https://api.spoonacular.com/recipes/"
 
 headers = {
-    "apiKey":api,
-    "ingredients":ingredients,
-    "number":8,
-    "sort":"min-missing-ingredients",
-    "ignorePantry":"false"
+    "apiKey":api,    
 }
 
 
 def search_by_ingredients(ingredients):
+    """
+	Searches for recipes by ingredients.
+
+	Parameters:
+	- ingredients: a list of strings representing the ingredients to search for.
+
+	Returns:
+	- meals: a dictionary with meal names as keys and a list of meal information as values. The meal information includes the meal image,meal id and a list of missing ingredients.
+	"""
 
     headers['ingredients'] = ingredients
+    headers['number'] = 5
+    headers['ignorePanrty'] = 'false'
+    headers['sort'] = "min-missing-ingredients"
 
     response = requests.get(f"{url}findByIngredients",params=headers)
     ic(response)
     data = response.json()
     ic(data)
-    meals = {} #dictionary with meal name,image and missing ingrdeints
+    meals = {} 
     for recipe in data:
         ic(recipe['title'])
-
+        ic(recipe['id'])
         missed_ingredients = []
         for ingredient in recipe["missedIngredients"]:
-            if ingredient['aisle'] == 'Meat': #removes meals in which Meat is needed and is missing
-                missed_ingredients.append("Break")
-
+          
             if ingredient['aisle'] == 'Spices and Seasonings': #pepper,salt..etc
                 continue
             missed_ingredients.append(ingredient["name"])
-        if len(missed_ingredients) <= 3 and "Break" not in missed_ingredients:
-            meals[recipe['title']] = [recipe['image'],missed_ingredients]
-        ic(f"for {recipe['title']} : {' and '.join(missed_ingredients)} are missing ingredients")
+        if len(missed_ingredients) <= 4:
+            meals[recipe['title']] = [recipe['image'],recipe['id'],missed_ingredients]
+
+        print(f"for {recipe['title']} : {' and '.join(missed_ingredients)} are missing ingredients")
         print()
     return meals
 
-meals = search_by_ingredients(ingredients)
-print(meals)
+
+
+
+
+meals = search_by_ingredients(ingredients) 
+ic(meals)
+
     
 
 
