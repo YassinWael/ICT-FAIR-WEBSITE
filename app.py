e14fe350e7094c3eed4dcaeebe52b4b164c66869
AHMEDDEV1224 <ahmedwaleedibrahim1224@gmail.com>
2024-01-20 22:35:27 +0300

    Homepage has been established and hosted using jinja2

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+if __name__ == '__main__':
+
+    app.run(debug=True,port=8080)
