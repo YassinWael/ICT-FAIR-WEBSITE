61363aff2690b552e75b4534ff16e5d87394b39c
YassinWael <yassinwaelmohamed84@gmail.com>
2024-02-01 20:13:10 +0300

    Made api call 6 seconds faster!

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # main.py
 from flask import Flask,request,render_template,make_response
 from dotenv import load_dotenv
-from os import getenv,environ
+from os import environ
 from functions import search_by_ingredients,chatgpt_info,get_quote
 from icecream import ic
 from json import dumps,loads
-
+load_dotenv('settings.env')
 spoonacular_api = environ.get('spoonacular')
 chatgpt_api = environ.get('OPENAI_API_KEY')
 app_key = environ.get('app_key')
@@ -89,13 +89,9 @@ def recipes():
 ic("Here")
 @app.route('/learn_more', methods=["GET","POST"])
 def learn_more():
-    ic("Here.2")
+ 
     meal_name = request.args.get('meal')
-    ic(meal_name)
-    past_meals = request.cookies.get('past_meals')
-
-    ic(past_meals)
-    
+    past_meals = request.cookies.get('past_meals')    
     if not past_meals: 
         past_meals = ["Eggs with cheese"]
     else:
@@ -110,9 +106,9 @@ def learn_more():
     json_list = dumps(past_meals) #Convert list to json string to send to page.
    
     
-    content = chatgpt_info(meal_name,recipes[meal_name])
     
-    response = make_response(render_template('learn_more.html',content=content,name = meal_name,image = recipes[meal_name][0],past_meals = past_meals))
+    
+    response = make_response(render_template('learn_more.html',content=chatgpt_info(meal_name,recipes[meal_name]),name = meal_name,image = recipes[meal_name][0],past_meals = past_meals))
     response.set_cookie('past_meals', json_list) 
     ic(json_list)
 
